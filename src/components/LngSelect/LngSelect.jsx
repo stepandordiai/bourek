978451b0fb2881c4d8753c29053d552f95218c8d
Stepan Dordiai <stepandordiai@gmail.com>
2025-11-22 23:29:08 +0100

    Update LngSelect with new svg icon

--- src/components/LngSelect/LngSelect.jsx
+++ src/components/LngSelect/LngSelect.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import i18n from "i18next";
-import downArrowIcon from "/icons/down-arrow.png";
 import "./LngSelect.scss";
 
 const lngData = [
@@ -69,7 +68,19 @@ const LanguageSelect = () => {
 						lngSelectActive ? "lng-select__btn-icon--active" : ""
 					}`}
 				>
-					<img width={20} height={20} src={downArrowIcon} alt="" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="20"
+						fill="#000000"
+						class="bi bi-chevron-down"
+						viewBox="0 0 16 16"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+						/>
+					</svg>
 				</span>
 			</button>
 			<ul

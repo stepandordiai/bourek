eff03b160063b58c1354083281dfda7c518f8796
Stepan Dordiai <stepandordiai@gmail.com>
2025-11-22 23:53:07 +0100

    Update Home with new svg icon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,6 @@ import { NavLink } from "react-router-dom";
 import Testimonials from "../../components/Testimonials/Testimonials";
 import FAQ from "../../components/FAQ/FAQ";
 import { HashLink } from "react-router-hash-link";
-import arrowUpperRightIcon from "/icons/arrow-upper-right.png";
 import "./Home.scss";
 
 const Home = () => {
@@ -65,21 +64,29 @@ const Home = () => {
 								.flatMap((link) => link.nestedLinks)
 								.map((link) => {
 									return (
-										<div key={link.id} className="home__service-card">
+										<NavLink
+											to={`/${link.id}`}
+											key={link.id}
+											className="home__service-card"
+										>
 											<p>{t(link.name)}</p>
-											<NavLink
-												className="grid-container__link"
-												to={`/${link.id}`}
-											>
+											<span className="grid-container__link">
 												<span>{t("home.find_out_more")}</span>
-												<img
-													src={arrowUpperRightIcon}
-													width={20}
-													height={20}
-													alt=""
-												/>
-											</NavLink>
-										</div>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="20"
+													height="20"
+													fill="currentColor"
+													class="bi bi-arrow-up-right"
+													viewBox="0 0 16 16"
+												>
+													<path
+														fill-rule="evenodd"
+														d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+													/>
+												</svg>
+											</span>
+										</NavLink>
 									);
 								})}
 						</div>

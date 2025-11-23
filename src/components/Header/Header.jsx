34c1b1463f4edc971b7e619bacd10b2f03805a16
Stepan Dordiai <stepandordiai@gmail.com>
2025-11-23 09:57:51 +0100

    Update Header with new svg icon and remove old png icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ import navLinksData from "./../../data/nav-links-data.json";
 import React from "react";
 import LngSelect from "../LngSelect/LngSelect";
 import addressesData from "./../../data/addresses-data.json";
-import facebookIcon from "/icons/facebook.png";
 import logo from "/logo/bourek.png";
 import "./Header.scss";
 
@@ -247,7 +246,16 @@ const Header = () => {
 							href="https://www.facebook.com/profile.php?id=61575622597789"
 							target="_blank"
 						>
-							<img width={20} height={20} src={facebookIcon} alt="" />
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="20"
+								height="20"
+								fill="currentColor"
+								class="bi bi-facebook"
+								viewBox="0 0 16 16"
+							>
+								<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+							</svg>
 						</a>
 					</footer>
 				</div>

c0eb29c1e374dca16b5c4870d976afbda0448f3c
Stepan Dordiai <stepandordiai@gmail.com>
2025-11-23 09:55:22 +0100

    Update fixed-link-container in Home with new svg icons

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,19 @@ function App() {
 						}`}
 						href="tel:+420602273579"
 					>
-						<img width={20} height={20} src={phoneCallIcon} alt="" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							class="bi bi-telephone-fill"
+							viewBox="0 0 16 16"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
+							/>
+						</svg>
 					</a>
 					<a
 						title="Facebook"
@@ -88,13 +100,31 @@ function App() {
 						href="https://www.facebook.com/profile.php?id=61575622597789"
 						target="_blank"
 					>
-						<img width={20} height={20} src={facebookIcon} alt="" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							class="bi bi-facebook"
+							viewBox="0 0 16 16"
+						>
+							<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+						</svg>
 					</a>
 					<button
 						onClick={toggleFloatContact}
 						className="fixed-link-container-btn"
 					>
-						<img width={20} height={20} src={dotsIcon} alt="" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							class="bi bi-three-dots-vertical"
+							viewBox="0 0 16 16"
+						>
+							<path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+						</svg>
 					</button>
 				</div>
 				<Banner />

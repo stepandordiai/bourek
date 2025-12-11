c823ede91fbd77e4a30baf5abcf81f8a65aa85e6
Stepan Dordiai <stepandordiai@gmail.com>
2025-12-11 14:00:40 +0100

    Update ScrollToTopBtn

--- src/components/ScrollToTopBtn/ScrollToTopBtn.jsx
+++ src/components/ScrollToTopBtn/ScrollToTopBtn.jsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
+import ArrowUpIcon from "../../icons/ArrowUpIcon";
 import "./ScrollToTopBtn.scss";
 
 const ScrollToTopBtn = () => {
@@ -32,19 +33,7 @@ const ScrollToTopBtn = () => {
 			aria-label={t("scrollToTop")}
 			title={t("scrollToTop")}
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="32"
-				height="32"
-				fill="currentColor"
-				className="bi bi-arrow-up-short"
-				viewBox="0 0 16 16"
-			>
-				<path
-					fillRule="evenodd"
-					d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"
-				/>
-			</svg>
+			<ArrowUpIcon />
 		</button>
 	);
 };
